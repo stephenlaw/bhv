73a774f040e5306ac166b074f61150cea00ff41b
Stephen Law <stephen.law@wur.nl>
2013-03-26 16:31:24 +0100

    #1 first init commit for packaging

---
+++ Scripts/bhv/Settings.js
@@ -0,0 +1,82 @@
+﻿/************************************************************/
+/** todo: add descriptions to all teh functions             */
+/** Done: First step was to organise the jscript functions  */
+/**                                                         */
+/************************************************************/
+
+var Settings = {
+    loadPreferences: function () {
+        if (App.debug)
+            console.log("Prefenties wordt aangeroepen");
+    },
+    savePreferences: function () {
+        window.localStorage.setItem('guestSwitch', true);
+
+        var contactData = $("#preferences-show-contactdata").data("kendoMobileSwitch").check();
+        window.localStorage.setItem('currentIdentity', contactData);
+
+        var webapiData = $("#preference-webapiurl").val();
+            window.localStorage.setItem('webapiurl', webapiData);
+
+        var identity = $("#preferences-identity").val();
+        window.localStorage.setItem('currentIdentity', identity);
+
+        //add feedback element next to the safe btn      
+        Utility.appendFeedback(".backbtn-wrapper", "<span class='feedback-right'>Voorkeuren zijn bewaard.</span>");
+
+        //delete it after 2 seconds
+        Utility.removeElement(2000, ".feedback-right");
+    },
+    // saveidentity doesn't have any reference. Is it going to be used? Ask gerrit
+    saveIdentity: function () {
+        var identityValue = $("#identity_label").val();
+        window.localStorage.setItem('currentIdentity', identityValue);
+        App.currentIdentity = identityValue;
+        //Reset van de applicatie moet eigenlijk plaatsvinden.
+    },
+    setPreferences: function () {
+        App.showGuestBuildings = true;
+        App.currentIdentity = "law002";
+        App.webapiurl = "http://localhost/bhv.api/api/";
+        //App.webapiurl = "http://bhvapi.seigers.nl/api/";
+
+        if (window.localStorage.getItem('guestSwitch') != null) {
+            App.showGuestBuildings = window.localStorage.getItem('guestSwitch');
+        }
+
+//        if (App.showGuestBuildings == "true") {
+//            App.buildingRequestType.Type = "all";
+//            $("#guest-location-switch").attr("checked", "checked");
+//        } else {
+//            App.buildingRequestType.Type = "assigned";
+//            $("#guest-location-switch").attr("checked", "unchecked");
+//        }
+
+        if (window.localStorage.getItem('currentIdentity') != null) {
+            App.currentIdentity = window.localStorage.getItem('currentIdentity');
+            $("#identity_label").val(App.currentIdentity);
+            $("#preferences-identity").val(App.currentIdentity);
+        } else {
+            App.currentIdentity = null;
+            $("#identity_label").val("Not Set");
+            $("#preferences-identity").val(App.currentIdentity);
+        }
+
+        if (window.localStorage.getItem('webapiurl') != null) {
+            App.webapiurl = window.localStorage.getItem('webapiurl');
+            $("#preference-webapiurl").val(App.webapiurl);
+        }
+
+        App.showContactData = true;
+        if (window.localStorage.getItem('show-contactdata') != null) {
+            var contactData = window.localStorage.getItem('show-contactdata');
+            if (contactData == "true") {
+                App.showContactData = true;
+                $("#preferences-show-contactdata").attr("checked", "checked");
+            } else {
+                App.showContactData = false;
+                $("#preferences-show-contactdata").attr("checked", "unchecked");
+            }
+        }
+    }
+}
